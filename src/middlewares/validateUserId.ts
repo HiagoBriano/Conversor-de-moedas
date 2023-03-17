fa2ac9305e085d42017294ce47a3b9ff44e132cd
Hiago Briano <hiago.artist@hotmail.com>
2023-03-17 16:33:40 -0300

    function that validates parameter created

--- src/middlewares/validateUserId.ts
+++ src/middlewares/validateUserId.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 const validateUserId = (req: Request, res: Response, next: NextFunction) => {
   const { userId } = req.params;
 
-  if (typeof parseInt(userId) !== 'number') {
+  if (!parseInt(userId)) {
     return res.status(400).json({ message: 'invalid userid' });
   }
 
