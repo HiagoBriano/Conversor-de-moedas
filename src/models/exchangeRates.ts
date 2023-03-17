29662bb6942a73da7834242e1d99fb3002cdffa6
Hiago Briano <hiago.artist@hotmail.com>
2023-03-17 15:41:01 -0300

    formatted with prettier

--- src/models/exchangeRates.ts
+++ src/models/exchangeRates.ts
@@ -2,22 +2,26 @@ import axios from 'axios';
 require('dotenv/config');
 
 interface ExchangeRates {
-  success: boolean,
-  query: { from: string, to: string, amount: number },
-  info: { timestamp: number, rate: number },
-  date: string,
-  result: number
+  success: boolean;
+  query: { from: string; to: string; amount: number };
+  info: { timestamp: number; rate: number };
+  date: string;
+  result: number;
 }
 
 interface Error {
   data: {
-    message: string,
-  }
+    message: string;
+  };
 }
 
 const key = process.env.API_KEY;
 
-const exchangeRates = async (to: string, from: string, amount: number): Promise<ExchangeRates | Error> => {
+const exchangeRates = async (
+  to: string,
+  from: string,
+  amount: number
+): Promise<ExchangeRates | Error> => {
   const response: ExchangeRates | Error = await axios
     .get(
       `https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,
