cb2f81378868777ee17e381d891e46b41632be63
Hiago Briano <hiago.artist@hotmail.com>
2023-03-17 15:51:25 -0300

    middlewares to identify error in the created server

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express from 'express';
+import serverError from './src/middlewares/serverError';
 require('dotenv/config');
 
 const app = express();
@@ -9,5 +10,6 @@ app.use(cors());
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
 
 app.get('/', (_req, res) => res.json({ message: 'active server' }));
+app.use(serverError);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
