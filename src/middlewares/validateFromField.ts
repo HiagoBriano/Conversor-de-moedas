3d00f927ea2384725a561bfb976631a245891630
Hiago Briano <hiago.artist@hotmail.com>
2023-03-17 16:10:41 -0300

    added supported currencies

--- src/middlewares/validateFromField.ts
+++ src/middlewares/validateFromField.ts
@@ -2,9 +2,19 @@ import { NextFunction, Request, Response } from 'express';
 
 const validateFromField = (req: Request, res: Response, next: NextFunction) => {
   const { from } = req.body;
+  const coins = ['BRL', 'USD', 'EUR', 'JPY'];
 
   if (!from) {
-    return res.status(400).json({ message: 'it is mandatory to send the field "from"' });
+    return res
+      .status(400)
+      .json({ message: 'it is mandatory to send the field "from"' });
+  } else if (!coins.includes(from)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          'the field "from" must be one of the following values: "BRL", "USD", "EUR", "JPY"',
+      });
   }
 
   next();
--- src/middlewares/validateToField.ts
+++ src/middlewares/validateToField.ts
@@ -2,9 +2,19 @@ import { NextFunction, Request, Response } from 'express';
 
 const validateToField = (req: Request, res: Response, next: NextFunction) => {
   const { to } = req.body;
+  const coins = ['BRL', 'USD', 'EUR', 'JPY'];
 
   if (!to) {
-    return res.status(400).json({ message: 'it is mandatory to send the field "to"' });
+    return res
+      .status(400)
+      .json({ message: 'it is mandatory to send the field "to"' });
+  } else if (!coins.includes(to)) {
+    return res
+      .status(400)
+      .json({
+        message:
+          'the field "to" must be one of the following values: "BRL", "USD", "EUR", "JPY"',
+      });
   }
 
   next();
