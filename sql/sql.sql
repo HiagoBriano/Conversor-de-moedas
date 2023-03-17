076268630a8d94caae522ca119019850bda9ac62
Hiago Briano <hiago.artist@hotmail.com>
2023-03-17 14:02:26 -0300

    docker-compose configured

---
+++ sql/sql.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS improving;
+USE improving;
+DROP TABLE IF EXISTS record;
+
+CREATE TABLE record (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  user_id int NOT NULL,
+  origin_currency varchar(50) NOT NULL,
+  origin_value int NOT NULL,
+  destination_currency varchar(255) NOT NULL,
+  conversion_rate_used  int NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP()
+) ENGINE=InnoDB;
