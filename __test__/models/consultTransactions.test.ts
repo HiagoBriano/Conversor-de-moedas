b9d910cffcc274619c2f1941b545b403bcb19c18
Hiago Briano <hiago.artist@hotmail.com>
2023-03-17 19:55:39 -0300

    Test for function consultTransactionsModel created

---
+++ __test__/models/consultTransactions.test.ts
@@ -0,0 +1,72 @@
+import consultTransactionsModel from '../../src/models/consultTransactions';
+import connection from '../../src/models/connection';
+
+jest.mock('../../src/models/connection', () => {
+  return {
+    execute: jest.fn(),
+  };
+});
+
+describe('checkDateModel', () => {
+  const date = new Date();
+
+  it('should return a date', async () => {
+    const mock = [
+      [
+        {
+          id: 1,
+          user_id: 10,
+          origin_currency: 'BRL',
+          origin_value: 5,
+          destination_currency: 'USD',
+          conversion_rate_used: 0.189422,
+          created_at: date,
+        },
+        {
+          id: 2,
+          user_id: 10,
+          origin_currency: 'BRL',
+          origin_value: 5,
+          destination_currency: 'USD',
+          conversion_rate_used: 0.189422,
+          created_at: date,
+        },
+      ],
+    ];
+
+    const response = [
+      {
+        id: 1,
+        user_id: 10,
+        origin_currency: 'BRL',
+        origin_value: 5,
+        destination_currency: 'USD',
+        conversion_rate_used: 0.189422,
+        created_at: date,
+      },
+      {
+        id: 2,
+        user_id: 10,
+        origin_currency: 'BRL',
+        origin_value: 5,
+        destination_currency: 'USD',
+        conversion_rate_used: 0.189422,
+        created_at: date,
+      },
+    ];
+    connection.execute.mockResolvedValue(mock);
+    const result = await consultTransactionsModel(10);
+
+    expect(result).toStrictEqual(response);
+  });
+
+  it('should return an error', async () => {
+    const mockExecute = jest.fn(() => {
+      throw new Error('error');
+    });
+
+    connection.execute.mockImplementationOnce(mockExecute);
+
+    await expect(consultTransactionsModel(10)).rejects.toThrowError();
+  });
+});
