fd478110cf409bdc4eff113d451c2295a9431a34
Hiago Briano <hiago.artist@hotmail.com>
2023-03-18 14:04:12 -0300

    Users table added to MySQL

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,6 +1,15 @@
 CREATE DATABASE IF NOT EXISTS improving;
 USE improving;
 DROP TABLE IF EXISTS record;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name varchar(50) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE,
+  password varchar(255) NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP()
+) ENGINE=InnoDB;
 
 CREATE TABLE record (
   id int AUTO_INCREMENT PRIMARY KEY,
@@ -9,5 +18,6 @@ CREATE TABLE record (
   origin_value DOUBLE NOT NULL,
   destination_currency varchar(255) NOT NULL,
   conversion_rate_used DOUBLE NOT NULL,
-  created_at timestamp DEFAULT CURRENT_TIMESTAMP()
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP(),
+  FOREIGN KEY (user_id) REFERENCES users(id)
 ) ENGINE=InnoDB;
